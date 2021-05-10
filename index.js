3671d10f43e203a13bf57647e4bb7772ed671278
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-05-10 17:44:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  for(let i = 0; i<sourceArray.length; i++){
+    sourceArray[i] = sourceArray[i]*(-1);
+  }
+  return sourceArray;
+}
+function mapToNoChange(sourceArray){
+  for(let i = 0; i<sourceArray.length; i++){
+    sourceArray[i] = sourceArray[i];
+  }
+  return sourceArray;
+}
+function mapToDouble(sourceArray){
+  for(let i = 0; i<sourceArray.length; i++){
+    sourceArray[i] = sourceArray[i]*2;
+  }
+  return sourceArray;
+}
+function mapToSquare(sourceArray){
+  for(let i = 0; i<sourceArray.length; i++){
+    sourceArray[i] = sourceArray[i]*sourceArray[i];
+  }
+  return sourceArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+  let sum = startingPoint;
+  for (let i=0;i<sourceArray.length;i++){
+    sum += sourceArray[i];
+  }
+  return sum;
+}
+function reduceToAllTrue(sourceArray){
+  for (let i=0;i<sourceArray.length;i++){
+    if(sourceArray === false) return false;
+    else return true;
+  }
+}
+function reduceToAnyTrue(sourceArray){
+  for (let i=0;i<sourceArray.length;i++){
+    if (sourceArray[i] === false) return false;
+  }
+  for (let i=0;i<sourceArray.length;i++){
+    if (sourceArray[i] === true) return true;
+    else return false;
+  } 
+}
